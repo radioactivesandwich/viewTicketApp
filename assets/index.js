dfd3a3398cf79bef7d3523a3edc86dfcbe4ac69e
radioactivesandwich <bpg62983@gmail.com>
2017-12-16 22:05:41 -0600

    Inserted FOR loop to list all tickets url and id elements in the tickets object array

---
+++ assets/index.js
@@ -0,0 +1,42 @@
+// Initialise the Zendesk JavaScript API client
+// https://developer.zendesk.com/apps/docs/apps-v2
+var client = ZAFClient.init();
+client.invoke('resize', { width: '100%', height: '200px' });
+
+//Set tickets endpoint and define GET call
+var fetchItem = {
+  url: 'https://z3ngoff.zendesk.com/api/v2/tickets.json',
+  cors: true,
+  type: 'GET',
+  dataType: 'json'
+};
+
+//Create function to initialize GET call
+function ticketFetch() {
+	var client = ZAFClient.init();
+
+	//Fetch tickets endpoint
+	client.request('/api/v2/tickets.json').then(
+	  function(tickets) {
+
+	  	//make sure tickets are being grabbed with console.log of tickets
+	    console.log(tickets);
+
+	    //Get number of tickets and set an array length minus 1
+		arrayLength = tickets.count - 1;
+
+	    //Go through array and make a list of the tickets
+	    for (var i = arrayLength; i >= 0; i--) { 
+		//push ticket array to app
+	    var z = document.createElement('p');
+		z.innerHTML = '<a href="' + tickets.tickets[i].url + '">' + "Ticket: " + tickets.tickets[i].id + "</a>";
+		document.getElementById("ticketList").appendChild(z);
+		}
+
+	  },
+	  function(response) {
+	    console.error(response.responseText);
+	  }
+	);
+}
+
