bc098595645468aa67184f2949607c2b89b98e45
radioactivesandwich <bpg62983@gmail.com>
2018-01-14 13:37:26 -0600

    researched looping through promises... so close. Also tweaked code a bit so it'll work with multiple pages. Was previously only working when 1 page of results was returned, which would be why it didn't work on my other larger test accounts.

--- assets/index.js
+++ assets/index.js
@@ -4,43 +4,50 @@ function ticketFetch() {
 	// Initialise the Zendesk JavaScript API client
 	var client = ZAFClient.init();
 
-	var url = '/api/v2/tickets.json';
+	var ticketsUrl = "/api/v2/tickets.json";
 
 	//Fetch tickets endpoint
-	client.request(url).then(
-	  function(tickets) {
+	client.request(ticketsUrl)
+		.then(function(tickets) {
 
 
 
-	  	//make sure tickets are being grabbed with console.log of tickets
-	    console.log(tickets.tickets[0].url);
+		  	//make sure tickets are being grabbed with console.log of tickets
+		    console.log(tickets.tickets[0].url);
 
-	    //Check to see what we're getting for next_page
-	    console.log("next_pagetest = " + tickets.next_page);
+		    //Check to see what we're getting for next_page
+		    console.log("next_pagetest = " + tickets.next_page);
+		    console.log(tickets.count);
 
-	    //Get number of tickets and set an array length minus 1
-		arrayLength = tickets.count - 1;
+		    //Get the length of the Tickets array and subtract 1
+			var arrayLength = tickets.tickets.length - 1;
+			console.log(tickets.tickets.length);
 
-	    //Go through array and make a list of the tickets
-	    for (var i = arrayLength; i >= 0; i--) { 
-		//push ticket array to app
-	    var z = document.createElement('p');
-		z.innerHTML = '<a href="' + tickets.tickets[i].url + '">' + "Ticket: " + tickets.tickets[i].id + "</a>";
-		document.getElementById("ticketList").appendChild(z);
+		    //Go through array and make a list of the tickets
+		    for (var i = arrayLength; i >= 0; i--) { 
+			//push ticket array to app
+		    var z = document.createElement('p');
+			z.innerHTML = '<a href="' + tickets.tickets[i].url + '">' + "Ticket: " + tickets.tickets[i].id + "</a>";
+			document.getElementById("ticketList").appendChild(z);
+			console.log("test: " + tickets.tickets[i].url)
 		}
 
-	  },
-	  function(response) {
-	    console.error(response.responseText);
-	  }
+		},
+		  function(response) {
+		    console.error(response.responseText);
+		  }
 	);
+
 }
 
 /* 
+Making progress. Everything is working. Now just need to figure out how to loop through this thing.
 
-Ideally you'd want to make one GET to the page, check to see if next_page equals null.
+function executes
+	for loop
+		promise  <-- need to figure out how to return value out of this
+	exit loop
+done
 
-If null, then move on. If not null, then run the function.
-
-
+Check out this promise loop: https://jsbin.com/wirumixojo/edit?html,js,console
 */
